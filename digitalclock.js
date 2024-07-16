6475cc06f02ff2a2d585867d53d9c1db900e0ed1
Lavanya Rajeswari <saridelavanyarajeswari@gmail.com>
2024-07-16 10:04:46 +0530

    first commit

---
+++ digitalclock.js
@@ -0,0 +1,8 @@
+function time(){
+    var x = new Date();
+    document.getElementsByClassName("hours")[0].innerHTML = x.getHours();
+    document.getElementsByClassName("minutes")[0].innerHTML = x.getMinutes();
+    document.getElementsByClassName("seconds")[0].innerHTML = x.getSeconds();
+}
+
+setInterval(time,1000);
